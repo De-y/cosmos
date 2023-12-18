300ca1ca896669f6603069809d3dd9718d65c6f2
Defy <61808223+De-y@users.noreply.github.com>
2023-12-18 14:56:56 -0500

    cosmic test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Cosmos
 ## Minimal webkit2 greeter theme for LightDM.
 
+### Credits
+Credits to [dimaglushkov's repository](https://github.com/dimaglushkov/lightdm-webkit2-theme-minimal) for the initial code and files from minimal for me to be able to make it and build upon it and make it better
---
+++ js/functions/datetime.js
@@ -0,0 +1,51 @@
+const language = 'en-US'
+function get_date_time() {
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = monthNames[now.getMonth()]; 
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds(); 
+
+    if(day.toString().length == 1) {
+         day = '0' + day;
+    }   
+    if(hour.toString().length == 1) {
+         hour = '0' + hour;
+    }
+    if(minute.toString().length == 1) {
+         minute = '0' + minute;
+    }
+    if(second.toString().length == 1) {
+         second = '0' + second;
+    }
+    if (language == 'en-US') {
+        return (day + ' ' + month + ' ' + year + ', ' + hour + ':' + minute);
+    } else {
+        return (month + ' ' + day + ' ' + year + ', ' + hour + ':' + minute);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {  
+     time = document.getElementById('time')
+     setInterval(() => {
+         time.innerHTML = get_date_time();
+     }, 1000);
+     document.getElementById('time').innerHTML = get_date_time();
+ 
+     if(window.lightdm !== undefined && lightdm.hostname !== undefined) {
+         document.getElementById('pagetitle').innerText = lightdm.hostname;
+     }
+ 
+     if(window.lightdm !== undefined && lightdm.users.length === 1) {
+         document.getElementById('username').value = lightdm.users[0].username;
+         document.getElementById('password').focus();
+     }
+ 
+     add_session_options()
+     init_session_menu()
+     
+ });
+ 
